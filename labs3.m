0f77ec986c1080793c8bc3c873f5617cb2fdec7c
Jasmine Lu <jasminelu.3@gmail.com>
2018-02-05 21:25:14 +0000

    clean up labs3

--- labs3.m
+++ labs3.m
@@ -3,46 +3,51 @@ function output = labs3(path, prefix, first, last, digits, suffix)
 % labs3('footage', 'footage_', 1, 657, 3, 'png')
 image_data = load_sequence(path, prefix, first, last, digits, suffix);
 image_data = im2double(image_data);
+corrected = image_data;
 
-% part i. detect scene cuts and add overlaying text
+% part i. detect scene cuts
 scene_cut_frames = detect_scene_cuts(image_data); % part i
 
-% part ii
-corrected_data = correct_flicker(image_data, 1, 100);
-corrected_data = correct_flicker(corrected_data, 257, 496);
-corrected_data = correct_flicker(corrected_data, 497, 657);
+start_frame = 1;
+scene_2 = scene_cut_frames(1);
+scene_3 = scene_cut_frames(2);
+last_frame = size(image_data,3);
+
+% part ii correct flicker
+corrected = correct_flicker(corrected, start_frame, scene_2-1);
+corrected = correct_flicker(corrected, scene_2, scene_3-1);
+corrected = correct_flicker(corrected, scene_3, last_frame);
 
 % part iii remove blotches
-% blotches = zeros(size(image_data));
-% [blotches, corrected_data] = fix_blotches(corrected_data, 1, 256, blotches);
-% [blotches, corrected_data] = fix_blotches(corrected_data, 257, 496, blotches);
-% [blotches, corrected_data] = fix_blotches(corrected_data, 497, 657, blotches);
+blotches = zeros(size(image_data));
+[blotches, corrected] = fix_blotches(corrected, start_frame, scene_2-1, blotches);
+[blotches, corrected] = fix_blotches(corrected, scene_2, scene_3-1, blotches);
+[~, corrected] = fix_blotches(corrected, scene_3, last_frame, blotches);
 
-% part iv
-% corrected_data = remove_v_artefacts(corrected_data, 497, 657); 
+% part iv correct vertical artefacts
+corrected = remove_v_artefacts(corrected, scene_3, last_frame); 
 
-% plot(1:476, image_data(200,:,657), 1:476, corrected_data(200,:,657));
-
-% corrected_data = camera_shake(corrected_data); % part v
+% part v correct camera shake
+corrected = camera_shake(corrected);
 
 % add overlaying text to scene cuts
 for n = 1 : size(scene_cut_frames,2)
     position = [10 20]; 
-%     text_str = cell(1,1);
-%     text_str{1} = 'Scene Cut!';
     for m = 0:15
-        curr_frame = image_data(:,:,scene_cut_frames(n)+m);
+        curr_frame = corrected(:,:,scene_cut_frames(n)+m);
         with_text = insertText(curr_frame,position,'Scene Cut!','FontSize',30); 
-        image_data(:,:,scene_cut_frames(n)+m) = rgb2gray(with_text);
+        corrected(:,:,scene_cut_frames(n)+m) = rgb2gray(with_text);
     end   
 end
 
-implay([image_data, corrected_data]);
+implay([image_data, corrected]);
+
+% plot(1:476, image_data(200,:,657), 1:476, corrected(200,:,657));
 
 % figure
 % subplot(1,2,1)
 % plot(1:476, image_data(200,:,510))
 % subplot(1,2,2)
-% plot(1:476, corrected_data(200,:,510))
+% plot(1:476, corrected(200,:,510))
 
 end
